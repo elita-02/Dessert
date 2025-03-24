80a19eac37fe6b6921bee3e5842db660995240b8
Talipov005 <talipovokuu@gmail.com>
2025-03-24 19:45:00 +0600

    talipov

--- kris_proect/src/pages/reteil/Reteil.jsx
+++ kris_proect/src/pages/reteil/Reteil.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
-import "./Reteil.scss"
+import React from 'react';
+import "./Reteil.scss";
+import tort from "../../assets/Img/tort.png";
+
 function Reteil() {
   return (
-    <div>
-      Продажи в рознице
+    <div className='glava'>
+<h1>Продажи в рознице</h1>
+    <div className="cards-container">
+      {Array(6).fill().map((_, index) => (
+        <div className="card" key={index}>
+          <img src={tort} alt="macarons" className="card-image" />
+          <div className="card-content">
+            <div className="left">
+              <h2 className="title">Коллаж</h2>
+              <p className="address">Амурская улица, 22А</p>
+              <p className="time">🕒 Пн - Пт 9:00 - 19:00</p>
+            </div>
+            <button className="reserve-btn">Забронировать продукцию</button>
+          </div>
+        </div>
+      ))}
     </div>
-  )
+    </div>
+  );
 }
 
-export default Reteil
+export default Reteil;
